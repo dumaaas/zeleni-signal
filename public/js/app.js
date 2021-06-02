fe105e362cb53a6679d491c058c104071aec0c9c
dumaaas <markodumnic8@gmail.com>
2021-06-02 02:43:31 +0200

    Dodata sekcija 3 - 8 tacaka koje vode do vozacke

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,13 @@
 //initializing new Wow object on load of window
 $(window).on('load', function() {
-    setTimeout(function() {
-        new WOW().init();
-        $(".loader").hide();
-        $(".showPage").fadeIn('slow');
-    }, 3800)
+    // setTimeout(function() {
+    //     new WOW().init();
+    //     $(".loader").hide();
+    //     $(".showPage").fadeIn('slow');
+    // }, 3800)
+    new WOW().init();
+    $(".loader").hide();
+    $(".showPage").fadeIn('slow');
 });
 
 function nesto() {
