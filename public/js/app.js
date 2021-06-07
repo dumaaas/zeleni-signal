9dc4b148c33dfb782d5969bf1a3a6a4009d9aa64
dumaaas <markodumnic8@gmail.com>
2021-06-07 17:37:45 +0200

    Smooth scroll, back to top btn, linked, changed header

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 //initializing new Wow object on load of window
-$(window).on('load', function() {
+$(window).on('load', function () {
     // setTimeout(function() {
     //     new WOW().init();
     //     $(".loader").hide();
@@ -10,21 +10,87 @@ $(window).on('load', function() {
 
     $(".loader").hide();
     $(".showPage").fadeIn('slow');
+
+    //to avoid exepction on Lighthouse report for passive listeners
+    jQuery.event.special.touchstart = {
+        setup: function (_, ns, handle) {
+            this.addEventListener("touchstart", handle, {
+                passive: true
+            });
+        }
+    };
+
+    //smooth scroling to the top of page
+    var btn = $('#top-btn');
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, '500');
+    });
+
+    //smooth scrolling on links of asside nav
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
 });
 
+$(window).on('scroll', function () {
+    var sticky = $('#header');
+    var scrollPos = $(window).scrollTop();
+
+    //adding fixed header with animation 
+    if (scrollPos > 34) {
+        sticky.addClass('header-top');
+    } else {
+        sticky.removeClass('header-top');
+    }
+
+    //changing links style of sections on scrolling
+    $('#header-menu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos  && refElement.position().top + refElement.height() > scrollPos ) {
+            $('#header-menu ul li a').removeClass("active-link");
+            currLink.addClass("active-link");
+        } else {
+            currLink.removeClass("active-link");
+        }
+    });
+
+    // showing to the top btn 
+    if ($(window).scrollTop() > 300) {
+        $('#top-btn').addClass('show');
+    } else {
+        $('#top-btn').removeClass('show');
+    }
+})
+
 //Trigering counter function on appear of section 13, only once
-$(function() {
+$(function () {
     var $triggered_times = true;
     $('.results-counter').appear();
-    $('.results-counter').on('appear', function() {
+    $('.results-counter').on('appear', function () {
         if ($triggered_times) {
-            $('.count').each(function() {
+            $('.count').each(function () {
                 $(this).prop('Counter', 0).animate({
                     Counter: $(this).text()
                 }, {
                     duration: 4000,
                     easing: 'swing',
-                    step: function(now) {
+                    step: function (now) {
                         $(this).text(Math.ceil(now));
                     }
                 })
@@ -35,14 +101,14 @@ $(function() {
 });
 
 
- //slider - Section 7
- $('.owl-carousel').owlCarousel({
+//slider - Section 7
+$('.owl-carousel').owlCarousel({
     nav: true,
     loop: true,
     stagePadding: 0,
     items: 1,
-    margin:0,
-    singleItem:true,
+    margin: 0,
+    singleItem: true,
     smartSpeed: 1200,
     autoplayHoverPause: true,
     navText: ["<img src='images/nav-left.webp' class='mx-auto' loading='lazy' alt='bildstudio'>", "<img src='images/nav-right.webp' class='mx-auto' loading='lazy' alt='bildstudio'>"],
