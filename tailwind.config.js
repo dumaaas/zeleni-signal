5c57ace83604355762f3d234d7e58f1667852ddc
dumaaas <markodumnic8@gmail.com>
2021-06-08 02:07:02 +0200

    prekinut od strane Bojana Vujanca, te ostah bez odradjenog posla veceras

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,21 +15,10 @@ module.exports = {
       }
     },
     screens: {
-      'sm': {
-        'max': '767px'
-      },
-      'md': {
-        'min': '768px',
-        'max': '991px'
-      },
-      'lg': {
-        'min': '992px',
-        'max': '1199px'
-      },
-      'xl': {
-        'min': '1200px'
-      },
-
+      'sm': {'max': '767px'},
+      'md': {'min': '768px', 'max': '991px'},
+      'lg': {'min': '992px', 'max': '1199px'},
+      'xl': {'min': '1200px'},
     }
   },
   variants: {
